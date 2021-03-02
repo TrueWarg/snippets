be3f1943fc8e2948a62010ecf903ffd56c7b7d9e
TrueWarg <32998850+TrueWarg@users.noreply.github.com>
2021-03-02 17:37:34 +0200

    Add bbox converters

--- bbox_convert.py
+++ bbox_convert.py
@@ -1,1 +1,15 @@
+import torch
 
+# todo add numpy analog also
+def xcycwh_to_xyxy(boxes: torch.Tensor) -> torch.Tensor:
+    return torch.cat((
+        boxes[:, :2] - boxes[:, 2:] / 2,
+        boxes[:, :2] + boxes[:, 2:] / 2
+    ), boxes.dim() - 1)
+
+
+def xyxy_to_xcycwh(boxes: torch.Tensor) -> torch.Tensor:
+    return torch.cat((
+        (boxes[:, :2] + boxes[:, 2:]) / 2,
+        boxes[:, 2:] - boxes[:, :2]
+    ), boxes.dim() - 1)
